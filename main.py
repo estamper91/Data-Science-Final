300b4f947580cab92853d0b023225bc423bad926
estamper91 <161373682+estamper91@users.noreply.github.com>
2024-12-04 17:59:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import 
+import sys
+sys.path.append('/Data-Science-Final/utils')
+from helper_functions import metric_calc
+sys.path.append('/Data-Science-Finals/src/data')
+import load_data
+import preprocess
+sys.path.append('/Data-Science-Finals/src/features')
+import build_features
+sys.path.append('/Data-Science-Finals/src/visualization')
+import visualize
+sys.path.append('/Data-Science-Finals/src/models')
+import train_model
+import evaluate_model
