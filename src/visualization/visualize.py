ab35de98429fceb1df4203a4585e6706ddf1813d
estamper91 <161373682+estamper91@users.noreply.github.com>
2024-12-04 13:34:42 -0500

    Update visualize.py

---
+++ src/visualization/visualize.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+mushrooms = pd.read_csv('Data-Science-Final/data/preprocessed/mushrooms_cleaned.csv')
+
+sns.scatterplot(data=mushrooms,x='stem_height',y='stem_width',hue='class',alpha=0.25)
+plt.savefig('sHeightvssWidth.jpg')
+plt.show()
+sns.scatterplot(data=mushrooms,x='stem_height',y='cap_diameter',hue='class',alpha=0.25)
+plt.savefig('sHeightvscDiameter.jpg')
+plt.show()
+sns.scatterplot(data=mushrooms,x='cap_diameter',y='stem_width',hue='class',alpha=0.25)
+plt.savefig('cDiametervssWidth.jpg')
+plt.show()
